e5172178497604f2eea133ccd8ec79606ea8f39e
vini <viniciuscipolli@gmail.com>
2021-07-13 10:52:24 -0300

    spring-mvc-aula03-atv02

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,8 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,18 +14,15 @@ import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
+	
+	@PersistenceContext
+	private EntityManager em;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Bomba de ar para encher pneus, com alavanca, 60 cm, Vonder");
-		pedido.setDescricao("Possui corpo e plataforma de apoio em plástico reforçado,"
-				+ " manômetro para indicação da pressão e mangueira plástica com 60 cm e bico.");
-		pedido.setUrlProduto("https://www.amazon.com.br/gp/product/B076YJ31QF/ref=ox_sc_act_image_1?smid=A1ZZFT5FULY4LN&psc=1");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/517mc5mbbGL._AC_SL1000_.jpg");
-		pedido.setDataEntrega(LocalDate.now());
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = em.createQuery("selec p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
